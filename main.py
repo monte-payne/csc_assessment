67e399c7641c275e094f2601d158a038a40afbe2
Monte Payne <84358400+monte-payne@users.noreply.github.com>
2021-06-27 04:18:02 +0000

    Component 1: Fixed my Start Button

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
 from tkinter import*
 from PIL import ImageTk, Image
 
+names_list = []
+
 class QuizStarter:
   def __init__(self, parent):
-    background_color="Yellow" 
-    #frame set up
-    self.quiz_frame = Frame(parent, bg = background_color, padx=10, pady=10)
+    background_color="Yellow"
+
+
+    self.quiz_frame = Frame(parent, bg = background_color, padx=5, pady=5)
     self.quiz_frame.grid()
 
     #Label widget for heading
-    self.heading_label = Label (self.quiz_frame, text = "SPORTS", font=("Helvetica", "30", "bold"), bg=background_color)
-    self.heading_label.grid(row=0)
+    self.heading_label = Label (self.quiz_frame, text = "SPORTS", font=("Helvetica", "30"), bg=background_color)
+    self.heading_label.grid(row=0, padx=10, pady=10)
+    
   
     #Start button
-    self.start_button = Button (self.quiz_frame, text = "START", bg="steelblue")
-    self.start_button.grid(row=2) 
+    self.start_button = Button (self.quiz_frame, text = "START", bg="steelblue", command=self.start)
+    self.start_button.grid(row=2, padx=5, pady=5) 
    
    #Exit button
-    self.exit_button = Button (self.quiz_frame, text = "EXIT", bg="Red")
-    self.exit_button.grid(row=3)
+    self.exit_button = Button (self.quiz_frame, text = "EXIT", bg="red", command=self.quiz_frame.destroy)
+    self.exit_button.grid(row=4, padx=5, pady=5)
 
-    #Picture 
+    #Picture resize
     self.picture_image = Image.open("Sports.jpeg")
     self.picture_image = self.picture_image.resize((500, 200), Image.ANTIALIAS)
     self.picture_image = ImageTk.PhotoImage(self.picture_image)
-
     self.image_label= Label(self.quiz_frame, image=self.picture_image)
-    self.image_label.grid(row=1)
+    self.image_label.grid(row=1, pady=5, padx=5)
+
     
+  def start(self):
+    self.quiz_frame.destroy()  
+    NameEnter(root)
 
 
+class NameEnter:
+  def __init__(self, parent):
+    background_color="Yellow"
 
-#************Starting point program************#
+    #Quiz Frame
+    self.quiz_frame = Frame(parent, bg = background_color, padx=100, pady=100)
+    self.quiz_frame.grid()
+
+    self.user_label = Label (self.quiz_frame, text = "Please enter your username", font=("Helvetica", "20"), bg=background_color)
+    self.user_label.grid(row=0, padx=5, pady=5)
+  
+    #Name Enter
+    self.entry_box=Entry(self.quiz_frame)
+    self.entry_box.grid(row=1, pady=5, padx=5)
+ 
+
 if __name__ == "__main__":
   root = Tk()
   root.title("Sports")
